5c9db71a6d5cabaabdb787bb319206e63e16eec5
toshibowie86@gmail.com <toshibowie86@gmail.com>
2022-10-29 18:48:51 +0900

    Add i18 routing

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  i18n: {
+    // providing the locales supported by your application
+    locales: ["en", "ja"],
+   //  default locale used when the non-locale paths are visited
+    defaultLocale: "en",
+    trailingSlash: true,
+  },
 }
 
 module.exports = nextConfig
