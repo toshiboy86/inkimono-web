810679f66444073611f084a0199c233533a32ddd
toshibowie86@gmail.com <toshibowie86@gmail.com>
2022-11-03 23:37:37 +0900

    Add TService

--- types/index.d.ts
+++ types/index.d.ts
@@ -13,3 +13,47 @@ declare module 'next/app' {
     Component: NextPageWithLayout<P>
   }
 }
+
+export type TService = {
+  fields: {
+    title: string
+    title_en: string
+    title_ja: string
+    // portfolioImages: [
+    //   { metadata: [Object], sys: [Object], fields: [Object] },
+    //   { metadata: [Object], sys: [Object], fields: [Object] },
+    //   { metadata: [Object], sys: [Object], fields: [Object] },
+    //   { metadata: [Object], sys: [Object], fields: [Object] }
+    // ],
+    price: number
+    description_en: { content: [{content: [{value: string}]}] },
+    description_ja: { content: [{content: [{value: string}]}] },
+    // serviceCategory: {
+    //   metadata: { tags: [] },
+    //   sys: {
+    //     space: [Object],
+    //     id: '2BBLKk9MoEzg0So6zZcxP1',
+    //     type: 'Entry',
+    //     createdAt: '2022-04-02T08:03:36.510Z',
+    //     updatedAt: '2022-11-03T11:04:49.658Z',
+    //     environment: [Object],
+    //     revision: 3,
+    //     contentType: [Object],
+    //     locale: 'en-US'
+    //   },
+    //   fields: { title: 'KIMONO EXPERIENCE + PHOTOSHOOT', order: 1 }
+    // },
+    order: number
+    serviceDetails: [{fields: { title_en: string, title_ja: string }}]
+    // serviceDetails: [
+    //   { metadata: [Object], sys: [Object], fields: [Object] },
+    //   { metadata: [Object], sys: [Object], fields: [Object] },
+    //   { metadata: [Object], sys: [Object], fields: [Object] },
+    //   { metadata: [Object], sys: [Object], fields: [Object] },
+    //   { metadata: [Object], sys: [Object], fields: [Object] },
+    //   { metadata: [Object], sys: [Object], fields: [Object] },
+    //   { metadata: [Object], sys: [Object], fields: [Object] }
+    // ],
+    reservation_url: string
+  }
+}
