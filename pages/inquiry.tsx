a56e285f3c0dfceedb55c07e4eee16e8fb849096
toshibowie86@gmail.com <toshibowie86@gmail.com>
2022-11-27 16:51:45 +0900

    Add navigator.clipboard for copy

--- pages/inquiry.tsx
+++ pages/inquiry.tsx
@@ -1,10 +1,14 @@
 import Head from 'next/head'
 import type { NextPage } from 'next'
 import Container from '@mui/material/Container'
+import Button from '@mui/material/Button'
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
 import EmailRoundedIcon from '@mui/icons-material/EmailRounded';
 import { useLocale } from '../src/hooks/useLocale'
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+
+const email = 'inkimono.com@gmail.com'
 
 const Portfolio: NextPage = () => {
   const { wi18n } = useLocale()
@@ -22,7 +26,8 @@ const Portfolio: NextPage = () => {
         <Box textAlign={'center'}  mt={4} mb={4}>
           <EmailRoundedIcon></EmailRoundedIcon>
           <Typography variant="body2">Feel free to send message to</Typography>
-          <Typography variant="h5">inkimono.com@gmail.com</Typography>
+          <Typography variant="h5">{email}</Typography>
+          <Button onClick={() => navigator.clipboard.writeText(email)}>Copy<ContentCopyIcon /></Button>
           <Typography variant="body2">お気軽に上記emailまでご連絡ください。</Typography>
         </Box>
       </Box>
