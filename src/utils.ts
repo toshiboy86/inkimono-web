7210ca676b5dd2c8148e07f4088fb8b791f1c3b5
toshibowie86@gmail.com <toshibowie86@gmail.com>
2023-10-01 22:48:16 +0900

    Move generateRandomImages()

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,10 @@
 export const convertFirstLetterCapital = (str: string) => {
   return str.charAt(0).toUpperCase() + str.slice(1)
 }
+
+export const generateRandomImages = async (images: string[], limit: number) => {
+  const myfun = function(x: string,y: string){
+    return 0.5 - Math.random()
+  }
+  return images.sort(myfun).slice(0, 3)
+}
