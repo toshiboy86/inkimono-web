ec686622dce2752550cbd705f97d5dd8070d53ad
toshiboy86 <toshibowie86@gmail.com>
2025-01-01 17:49:09 +0900

    Add fetchPageByPageType and fetchPortfolioImagesById2

--- src/entities/repositories.ts
+++ src/entities/repositories.ts
@@ -1,34 +1,65 @@
-import { Asset } from "contentful";
-import { IServiceFields, IService, IServiceCategories, IServiceCategoriesFields, IServiceOptionFields, IServiceOption, IDescriptionFields, IDescription, ILocationsFields, ILocations, ILocationFields, ILocation, IQuestionFields, IQuestion, IServiceDetailFields, IServiceDetail } from "../../@types/generated/contentful"
+import { Asset } from 'contentful';
+import {
+  IServiceFields,
+  IService,
+  IServiceCategories,
+  IServiceCategoriesFields,
+  IServiceOptionFields,
+  IServiceOption,
+  IDescriptionFields,
+  IDescription,
+  IPageFields,
+  IPage,
+  ILocationsFields,
+  ILocations,
+  ILocationFields,
+  ILocation,
+  IQuestionFields,
+  IQuestion,
+  IServiceDetailFields,
+  IServiceDetail,
+} from '../../@types/generated/contentful';
 
 export type TGetApi = {
-  table: string,
-  query?: object,
-  option?: object
-}
+  table: string;
+  query?: object;
+  option?: object;
+};
 
-export type TServiceRepository = ({ fields: IServiceFields } & IService)
+export type TServiceRepository = { fields: IServiceFields } & IService;
 
-export type TService = IServiceFields & { id: string }
+export type TService = IServiceFields & { id: string };
 
-export type TServiceById = Record<string, TService>
+export type TServiceById = Record<string, TService>;
 
-export type TServiceCategoryRepository = ({ fields: IServiceCategoriesFields } & IServiceCategories)
+export type TServiceCategoryRepository = {
+  fields: IServiceCategoriesFields;
+} & IServiceCategories;
 
-export type TServiceCategory = IServiceCategoriesFields & { id: string }
+export type TServiceCategory = IServiceCategoriesFields & { id: string };
 
-export type TServiceDetailRepository = ({ fields: IServiceDetailFields } & IServiceDetail)
+export type TServiceDetailRepository = {
+  fields: IServiceDetailFields;
+} & IServiceDetail;
 
-export type TServiceDetail = Record<string, IServiceDetailFields>
+export type TServiceDetail = Record<string, IServiceDetailFields>;
 
-export type TServiceOptionRepository = ({ fields: IServiceOptionFields } & IServiceOption)
+export type TServiceOptionRepository = {
+  fields: IServiceOptionFields;
+} & IServiceOption;
 
-export type TDescriptionRepository = ({ fields: IDescriptionFields } & IDescription)
+export type TDescriptionRepository = {
+  fields: IDescriptionFields;
+} & IDescription;
 
-export type TLocationsRepository = ({ fields: ILocationsFields } & ILocations )
+export type TLocationsRepository = { fields: ILocationsFields } & ILocations;
 
-export type TLocationRepository = ({ fields: ILocationFields } & ILocation )
+export type TLocationRepository = { fields: ILocationFields } & ILocation;
 
-export type TQAndARepository = ({ fields: IQuestionFields } & IQuestion )
+export type TPageRepository = { fields: IPageFields } & IPage;
 
-export type TImages = { items: Asset[] }
+export type TPage = IPageFields & { id: string };
+
+export type TQAndARepository = { fields: IQuestionFields } & IQuestion;
+
+export type TImages = { items: Asset[] };
--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -1,76 +1,117 @@
-import { TDescriptionRepository, TLocationRepository, TLocationsRepository, TQAndARepository, TServiceCategoryRepository, TServiceDetailRepository, TServiceOptionRepository, TServiceRepository } from '../entities/repositories'
-import { fetchApi, fetchAsset } from './apiAdapter'
+import {
+  TDescriptionRepository,
+  TLocationRepository,
+  TLocationsRepository,
+  TPageRepository,
+  TQAndARepository,
+  TServiceCategoryRepository,
+  TServiceDetailRepository,
+  TServiceOptionRepository,
+  TServiceRepository,
+} from '../entities/repositories';
+import { fetchApi, fetchAsset } from './apiAdapter';
 
 export const fetchPortfolioImages = async (): Promise<string[]> => {
-  const images = await fetchAsset()
-  return images.data.items.map((image) => image.fields.file?.url) as string[]
-}
+  const images = await fetchAsset();
+  return images.data.items.map((image) => image.fields.file?.url) as string[];
+};
+
+export const fetchPortfolioImagesById2 = async (id: string) => {
+  const images = await fetchAsset({ query: { id } });
+  let imagesObj: Record<string, string> = {};
+
+  images.data.items.forEach((image) => {
+    const id = image.sys.id as string;
+    imagesObj[id] = image.fields.file?.url as string;
+  });
+  return imagesObj;
+};
 
 export const fetchPortfolioImagesById = async () => {
-  const images = await fetchAsset()
-  let imagesObj: Record<string, string> = {}
-  
+  const images = await fetchAsset();
+  let imagesObj: Record<string, string> = {};
+
   images.data.items.forEach((image) => {
-    const id = image.sys.id as string
-    imagesObj[id] = image.fields.file?.url as string
-  })
-  return imagesObj
-}
+    const id = image.sys.id as string;
+    imagesObj[id] = image.fields.file?.url as string;
+  });
+  return imagesObj;
+};
 
 export const fetchServices = async (): Promise<TServiceRepository[]> => {
   const res2 = await fetchApi<TServiceRepository[]>({
     table: 'service',
-    option: { order: 'fields.order' }
-  })
-  return res2.data
-}
+    option: { order: 'fields.order' },
+  });
+  return res2.data;
+};
 
-export const fetchServiceCategories = async (): Promise<TServiceCategoryRepository[]> => {
+export const fetchServiceCategories = async (): Promise<
+  TServiceCategoryRepository[]
+> => {
   const res = await fetchApi<TServiceCategoryRepository[]>({
     table: 'serviceCategories',
-    option: { order: 'fields.order' }
-  })
-  return res.data
-}
+    option: { order: 'fields.order' },
+  });
+  return res.data;
+};
 
-export const fetchServiceOptions = async (): Promise<TServiceOptionRepository[]> => {
+export const fetchServiceOptions = async (): Promise<
+  TServiceOptionRepository[]
+> => {
   const res = await fetchApi<TServiceOptionRepository[]>({
     table: 'serviceOption',
-  })
-  return res.data
-}
+  });
+  return res.data;
+};
 
-export const fetchDescriptions = async (): Promise<TDescriptionRepository[]> => {
+export const fetchDescriptions = async (): Promise<
+  TDescriptionRepository[]
+> => {
   const res = await fetchApi<TDescriptionRepository[]>({
     table: 'description',
-  })
-  return res.data
-}
+  });
+  return res.data;
+};
 
-export const fetchServiceDetails = async (): Promise<TServiceDetailRepository[]> => {
+export const fetchServiceDetails = async (): Promise<
+  TServiceDetailRepository[]
+> => {
   const res = await fetchApi<TServiceDetailRepository[]>({
     table: 'serviceDetail',
-  })
-  return res.data
-}
+  });
+  return res.data;
+};
 
 export const fetchLocations = async (): Promise<TLocationsRepository[]> => {
   const res = await fetchApi<TLocationsRepository[]>({
     table: 'locations',
-  })
-  return res.data
-}
+  });
+  return res.data;
+};
 
 export const fetchLocation = async (): Promise<TLocationRepository[]> => {
   const res = await fetchApi<TLocationRepository[]>({
     table: 'location',
-  })
-  return res.data
-}
+  });
+  return res.data;
+};
 
 export const fetchQuestionAndAnswer = async (): Promise<TQAndARepository[]> => {
   const res = await fetchApi<TQAndARepository[]>({
     table: 'question',
-  })
-  return res.data
-}
+  });
+  return res.data;
+};
+
+export const fetchPageByPageType = async (
+  type: string
+): Promise<TPageRepository[]> => {
+  const res = await fetchApi<TPageRepository[]>({
+    table: 'page',
+    query: {
+      'fields.pageType': type,
+    },
+  });
+  return res.data;
+};
