2ead657a1ad4dad78ede2d083432c0033855c497
toshiboy86 <toshibowie86@gmail.com>
2025-01-01 22:50:38 +0900

    Add price table in Workshop page

--- app/[lang]/workshop/workshop-page.tsx
+++ app/[lang]/workshop/workshop-page.tsx
@@ -1,6 +1,7 @@
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
+import Image from 'next/image';
 import Inquiry from '../../../components/Inquiry';
 import * as repositories from '../../../src/repositories';
 import { getDictionary } from '../dictionaries';
@@ -12,25 +13,31 @@ import { Grid } from '@mui/material';
 import Article from '../../../components/Typography/Article';
 import TopImage from '../../../components/TopImage';
 import { fetchPortfolioImagesById2 } from '../../../src/repositories';
+import { DEFAULT_IMAGE } from '../../../src/const';
 
 export default async function WorkshopPage(params: { lang: TLocale }) {
   const locale = params.lang;
   const dict = await getDictionary(locale);
   const [workshop] = await repositories.fetchPageByPageType('workshop');
+
   const mainDescription = workshop.fields[`description_${locale}_1`];
   const mainDescTitle = workshop.fields[`desc_title_${locale}_1`];
   const mainDescImage = workshop.fields.image_1;
 
-  const images = await fetchPortfolioImagesById2(
-    mainDescImage?.sys.id as string
-  );
+  const images = mainDescImage?.sys.id
+    ? await fetchPortfolioImagesById2(mainDescImage?.sys.id as string)
+    : null;
+
+  const second_images = workshop.fields.image_2?.sys.id
+    ? await fetchPortfolioImagesById2(workshop.fields.image_2?.sys.id as string)
+    : null;
 
   return (
     <div>
       <TopImage title="Workshop" />
       <Container maxWidth="lg">
         <Article
-          imageSrc={Object.values(images)[0]}
+          imageSrc={images ? Object.values(images)[0] : DEFAULT_IMAGE}
           imageAlt="Stasia for the workshop"
         >
           <Grid item xs={12} md={7}>
@@ -42,6 +49,29 @@ export default async function WorkshopPage(params: { lang: TLocale }) {
             </Box>
           </Grid>
         </Article>
+        <Grid xs={12}>
+          <Box sx={{ mt: 6 }}>
+            <Typography variant="h4" mb={2}>
+              Price
+            </Typography>
+            <Image
+              unoptimized
+              src={
+                second_images
+                  ? `${Object.values(second_images)[0]}?w=700&h=1000&q=75`
+                  : DEFAULT_IMAGE
+              }
+              alt={`Workshop price`}
+              width={700}
+              height={1000}
+              loading="lazy"
+              style={{
+                width: '100%',
+                height: 'auto',
+              }}
+            />
+          </Box>
+        </Grid>
         <Box textAlign={'center'} pt={3}>
           <Typography variant="h4">
             {dict['translation']['general']['inquiry']}
