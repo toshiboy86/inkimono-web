9ff8db36989a8da319653c4c1c3fe4b58e43d717
toshiboy86 <toshibowie86@gmail.com>
2025-01-01 17:48:57 +0900

    fetchAsset takes query param

--- src/repositories/apiAdapter.ts
+++ src/repositories/apiAdapter.ts
@@ -1,50 +1,62 @@
-import { TGetApi, TImages } from '../entities/repositories'
+import { TGetApi, TImages } from '../entities/repositories';
 
-export const fetchAsset = async (): Promise<{data: TImages}> => {
+export const fetchAsset = async (param?: {
+  query?: { id?: string };
+}): Promise<{ data: TImages }> => {
   const res = await fetch(
-    `https://cdn.contentful.com/spaces/${process.env.CTF_SPACE_ID}/environments/master/assets?limit=1000`,
+    `https://cdn.contentful.com/spaces/${
+      process.env.CTF_SPACE_ID
+    }/environments/master/assets?limit=1000${
+      param?.query?.id ? `&sys.id[in]=${param.query.id}` : ''
+    }`,
     {
       credentials: 'omit',
-      headers: { Authorization: `Bearer ${process.env.CTF_CDA_ACCESS_TOKEN}` }
+      headers: { Authorization: `Bearer ${process.env.CTF_CDA_ACCESS_TOKEN}` },
     }
-  )
+  );
 
-  if(res.ok){
+  if (res.ok) {
     const data = await res.json();
 
-    return {data: data}
+    return { data: data };
   } else {
-    throw new Error('error')
+    throw new Error('error');
   }
-}
+};
 
-export const fetchApi = async <T>(param: TGetApi): Promise<{data: T}> => {
-  let query: string = ''
+export const fetchApi = async <T>(param: TGetApi): Promise<{ data: T }> => {
+  let query: string = '';
 
-  const queryOption = {...param.option, include: '2'}
-  
+  const queryOption = { ...param.option, include: '2' };
+
   if (queryOption) {
-    const obj = queryOption as {[key: string]: string}
+    const obj = queryOption as { [key: string]: string };
 
-    query = Object.keys(queryOption).map((e) => {
-      return `${e}=${obj[e]}`
-    }).join('&')
+    query = Object.keys(queryOption)
+      .map((e) => {
+        return `${e}=${obj[e]}`;
+      })
+      .join('&');
   }
-
 
   const res = await fetch(
     `https://cdn.contentful.com/spaces/${process.env.CTF_SPACE_ID}/environments/master/entries?content_type=${param.table}&${query}`,
     {
       credentials: 'omit',
-      headers: { Authorization: `Bearer ${process.env.CTF_CDA_ACCESS_TOKEN}` },
-      next: { revalidate: false },
+      headers: {
+        Authorization: `Bearer ${process.env.CTF_CDA_ACCESS_TOKEN}`,
+        'Cache-Control': 'no-cache',
+        Pragma: 'no-cache',
+      },
+      next: { revalidate: 0 },
+      cache: 'no-store',
     }
-  )
+  );
 
-  if(res.ok){
+  if (res.ok) {
     const data = await res.json();
-    return {data: data.items as unknown as T}
+    return { data: data.items as unknown as T };
   } else {
-    return {data: `${res.status}: ${res.statusText}` as unknown as T}
+    return { data: `${res.status}: ${res.statusText}` as unknown as T };
   }
-}
+};
