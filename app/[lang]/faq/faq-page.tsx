65f8f680c12ec0348c658a2bc20e1f4428349100
toshiboy86 <toshibowie86@gmail.com>
2025-01-01 20:43:11 +0900

    Activate FAQ page

--- app/[lang]/faq/faq-page.tsx
+++ app/[lang]/faq/faq-page.tsx
@@ -1,65 +1,59 @@
-'use client'
-import Head from 'next/head'
-import Box from '@mui/material/Box'
+import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
-import Container from '@mui/material/Container'
+import Container from '@mui/material/Container';
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import { TLocale } from "../../../src/entities"
-import { fetchQuestionAndAnswer } from "../../../src/repositories"
-import { getDictionary } from '../dictionaries'
+import { TLocale } from '../../../src/entities';
+import { fetchQuestionAndAnswer } from '../../../src/repositories';
+import { getDictionary } from '../dictionaries';
 import TopImage from '../../../components/TopImage';
 import Inquiry from '../../../components/Inquiry';
-
-type TQnA = {
-  fields: {
-    question_ja: string,
-    question_en: string,
-    answer_en: string,
-    answer_ja: string,
-  }
-}
+import { IQuestionFields } from '../../../@types/generated/contentful';
 
 // TODO: Under migration.
 export default async function FAQPage(params: { lang: TLocale }) {
-  // const faq = await fetchQuestionAndAnswer()
-  // const locale = params.lang
-  // const dict = await getDictionary(locale)
+  const faq = await fetchQuestionAndAnswer();
+  const locale = params.lang;
 
   return (
     <div>
       <h1>Under Preparation</h1>
-      {/* <TopImage title='FAQ' />
+      <TopImage title="FAQ" />
       <Container maxWidth="lg">
         <Grid container spacing={1} sx={{ mt: 3 }}>
-        {
-          props.faq.map((e: TQnA) => {
-            if ((getCurrentLocale() === 'en' && e.fields.question_en) || getCurrentLocale() === 'ja' && e.fields.question_ja)
-            return (
-              <>
-                <Accordion
-                  sx={{
-                    width: 'inherit'
-                  }}
-                >
-                  <AccordionSummary
-                    expandIcon={<ExpandMoreIcon />}
-                    aria-controls="panel1a-content"
-                    id="panel1a-header"
-                  >
-                    <Typography>{getWordsOnLocale(e.fields, 'question')}</Typography>
-                  </AccordionSummary>
-                  <AccordionDetails>
-                    <Typography dangerouslySetInnerHTML={{__html: getWordsOnLocale(e.fields, 'answer').replace('/n', '<br>')}} />
-                  </AccordionDetails>
-                </Accordion>
-              </>
+          {faq.map((e: { fields: IQuestionFields }) => {
+            if (
+              (params.lang === 'en' && e.fields.question_en) ||
+              (params.lang === 'ja' && e.fields.question_ja)
             )
-          })
-        }
+              return (
+                <>
+                  <Accordion
+                    sx={{
+                      width: 'inherit',
+                    }}
+                  >
+                    <AccordionSummary
+                      expandIcon={<ExpandMoreIcon />}
+                      aria-controls="panel1a-content"
+                      id="panel1a-header"
+                    >
+                      <Typography>
+                        {e.fields.question_en || e.fields.question_ja}
+                      </Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                      <Typography>
+                        {e.fields.answer_en || e.fields.answer_ja}
+                      </Typography>
+                    </AccordionDetails>
+                  </Accordion>
+                </>
+              );
+          })}
         </Grid>
       </Container>
       <Box
@@ -69,12 +63,14 @@ export default async function FAQPage(params: { lang: TLocale }) {
           backgroundColor: 'rgba(48, 37, 37,0.9)',
           backgroundBlendMode: 'multiply',
           'object-fit': 'cover',
-          'height': {
+          height: {
             lg: '300px',
-          }
-        }}>
-        <Inquiry />
-      </Box> */}
+          },
+        }}
+      >
+        {/* @ts-expect-error Server Component */}
+        <Inquiry lang={params.lang} />
+      </Box>
     </div>
-  )
+  );
 }
