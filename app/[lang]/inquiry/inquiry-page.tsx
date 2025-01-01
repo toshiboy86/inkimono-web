e85952f8598938e72bc082e2302bfeb142ff7f51
toshiboy86 <toshibowie86@gmail.com>
2025-01-01 19:43:43 +0900

    Update inquiry page

--- app/[lang]/inquiry/inquiry-page.tsx
+++ app/[lang]/inquiry/inquiry-page.tsx
@@ -1,23 +1,32 @@
-'use client'
-import { Container, Box, Typography, Button } from "@mui/material"
-import EmailRoundedIcon from '@mui/icons-material/EmailRounded'
-import ContentCopyIcon from '@mui/icons-material/ContentCopy'
-import { TLocale } from "../../../src/entities"
+'use client';
+import { Container, Box, Typography, Button } from '@mui/material';
+import EmailRoundedIcon from '@mui/icons-material/EmailRounded';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import { TLocale } from '../../../src/entities';
 
 export default async function InquiryPage(params: { lang: TLocale }) {
-  const email = 'inkimono.com@gmail.com'
+  const email = 'inkimono.com@gmail.com';
 
   return (
     <Container>
       <Box>
-        <Box textAlign={'center'}  mt={4} mb={4}>
+        <Box textAlign={'center'} mt={4} mb={4}>
           <EmailRoundedIcon></EmailRoundedIcon>
-          <Typography variant="body2">Feel free to send message to</Typography>
+          <Typography variant="body2">Please send your inquiry to</Typography>
           <Typography variant="h5">{email}</Typography>
-          <Button onClick={() => navigator.clipboard.writeText(email)}>Copy<ContentCopyIcon /></Button>
-          <Typography variant="body2">お気軽に上記emailまでご連絡ください。</Typography>
+          <Button onClick={() => navigator.clipboard.writeText(email)}>
+            Copy
+            <ContentCopyIcon />
+          </Button>
+          <Typography variant="body2">
+            Please include the preferred time and dates and the number of
+            participants.
+          </Typography>
+          <Typography variant="body2">
+            (ご希望のお時間、日付、人数をお伝えください)
+          </Typography>
         </Box>
       </Box>
     </Container>
-  )
+  );
 }
