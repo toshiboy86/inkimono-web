927729cb2da078cb751248cc86a1ca0c6d06d79b
toshiboy86 <toshibowie86@gmail.com>
2025-01-01 19:44:04 +0900

    Update based on biome format

--- app/[lang]/location/location-page.tsx
+++ app/[lang]/location/location-page.tsx
@@ -1,43 +1,54 @@
-import { Box, Container, Grid } from '@mui/material'
-import TopImage from '../../../components/TopImage'
-import { TLocale } from '../../../src/entities'
-import { fetchLocations, fetchLocation, fetchPortfolioImagesById } from '../../../src/repositories'
-import LocationCard from '../../../components/LocationCard'
-import { getWordsOnLocale } from '../../../src/utils'
+import { Box, Container, Grid } from '@mui/material';
+import TopImage from '../../../components/TopImage';
+import { TLocale } from '../../../src/entities';
+import {
+  fetchLocations,
+  fetchLocation,
+  fetchPortfolioImagesById,
+} from '../../../src/repositories';
+import LocationCard from '../../../components/LocationCard';
+import { getWordsOnLocale } from '../../../src/utils';
 
 export default async function LocationPage(params: { lang: TLocale }) {
-  const locale = params.lang
-  const locations = await fetchLocations()
-  const location = await fetchLocation()
-  const images = await fetchPortfolioImagesById()
+  const locale = params.lang;
+  const locations = await fetchLocations();
+  const location = await fetchLocation();
+  const images = await fetchPortfolioImagesById();
 
   return (
     <div>
-      <TopImage title='Location' />
+      <TopImage title="Location" />
       <Container maxWidth="lg">
-        <Grid container spacing={1} textAlign='center' sx={{ mt: 3 }}>
+        <Grid container spacing={1} textAlign="center" sx={{ mt: 3 }}>
           <Grid item>
-            <Box
-              sx={{ pt: 3 }}
-            >
-            <Box fontSize={16} color="text.secondary">
-              {getWordsOnLocale(location[0].fields, 'description', locale).content.map((f: any) => f.content.map((e: any) => e.value).map((e: any) => e)).join('')}
-            </Box>
+            <Box sx={{ pt: 3 }}>
+              <Box fontSize={16} color="text.secondary">
+                {getWordsOnLocale(location[0].fields, 'description', locale)
+                  .content.map((f: any) =>
+                    f.content.map((e: any) => e.value).map((e: any) => e)
+                  )
+                  .join('')}
+              </Box>
             </Box>
           </Grid>
         </Grid>
-        <Grid container spacing={1} sx={{ mt: 3 }} >
+        <Grid container spacing={1} sx={{ mt: 3 }}>
           {locations.map((loc: any) => {
             return (
               <>
-              <Grid item xs={12} md={6}>
-                <LocationCard images={images} location={loc} locale={locale} key={loc.sys.id}></LocationCard>
-              </Grid>
+                <Grid item xs={12} md={6}>
+                  <LocationCard
+                    images={images}
+                    location={loc}
+                    locale={locale}
+                    key={loc.sys.id}
+                  ></LocationCard>
+                </Grid>
               </>
-            )
+            );
           })}
         </Grid>
       </Container>
     </div>
-  )
+  );
 }
--- components/Inquiry.tsx
+++ components/Inquiry.tsx
@@ -1,33 +1,42 @@
-import Link from 'next/link'
-import Container from '@mui/material/Container'
-import Box from '@mui/material/Box'
-import Typography from '@mui/material/Typography'
-import { Button } from '@mui/material'
-import { getDictionary } from '../app/[lang]/dictionaries'
-import { TLocale } from '../src/entities'
+import Link from 'next/link';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import { Button } from '@mui/material';
+import { getDictionary } from '../app/[lang]/dictionaries';
+import { TLocale } from '../src/entities';
 
 const Inquiry = async (params: { lang: TLocale }) => {
-  const dict = await getDictionary(params.lang)
+  const dict = await getDictionary(params.lang);
   return (
     <Container>
       <Box>
-        <Box textAlign={'center'} pt={9} sx={{ display: 'table-cell', verticalAlign: 'middle', color: '#f8fffc', fontWeight: '900'}}>
+        <Box
+          textAlign={'center'}
+          pt={9}
+          sx={{
+            display: 'table-cell',
+            verticalAlign: 'middle',
+            color: '#f8fffc',
+            fontWeight: '900',
+          }}
+        >
           <Typography variant="h4">
-            { dict['translation']['general']['inquiry'] }
+            {dict['translation']['general']['inquiry']}
           </Typography>
           <Box mt={4} mb={4}>
-            { dict['translation']['index']['inquiry_body_1'] }
-            { dict['translation']['index']['inquiry_body_2'] }
+            {dict['translation']['index']['inquiry_body_1']}
+            {dict['translation']['index']['inquiry_body_2']}
           </Box>
-          <Link href='/inquiry'>
+          <Link href="/inquiry">
             <Button variant="contained">
-              { dict['translation']['general']['inquiry'] }
+              {dict['translation']['general']['inquiry']}
             </Button>
           </Link>
-          </Box>
         </Box>
+      </Box>
     </Container>
-  )
-}
+  );
+};
 
-export default Inquiry
+export default Inquiry;
