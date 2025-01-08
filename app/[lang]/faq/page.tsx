f9c39772a4290d18391eb50e025531075926e0b2
toshiboy86 <toshibowie86@gmail.com>
2025-01-08 22:49:15 +0900

    Add canonical

--- app/[lang]/faq/page.tsx
+++ app/[lang]/faq/page.tsx
@@ -1,27 +1,30 @@
-import { Metadata } from 'next'
-import { TLocale, TMetadataProps } from '../../../src/entities'
-import FaqPage from './faq-page'
-import { getDictionary } from '../dictionaries'
+import { Metadata } from 'next';
+import { TLocale, TMetadataProps } from '../../../src/entities';
+import FaqPage from './faq-page';
+import { getDictionary } from '../dictionaries';
 
-export async function generateMetadata(
-  { params }: TMetadataProps,
-): Promise<Metadata> {
-  const dict = await getDictionary(params.lang)
+export async function generateMetadata({
+  params,
+}: TMetadataProps): Promise<Metadata> {
+  const dict = await getDictionary(params.lang);
   return {
     title: dict['translation']['meta']['faq_title'],
     description: dict['translation']['meta']['faq_description'],
     openGraph: {
       images: [dict['translation']['meta']['og_images']],
     },
-  }
+    alternates: {
+      canonical: '/faq',
+    },
+  };
 }
 export default function Page(params: { params: { lang: TLocale } }) {
-    // TODO: params is added by next.js
-  const locale = (params as any).params.lang as TLocale
+  // TODO: params is added by next.js
+  const locale = (params as any).params.lang as TLocale;
   return (
     <>
       {/* @ts-expect-error Server Component */}
-      <FaqPage lang={locale}/>
+      <FaqPage lang={locale} />
     </>
-  )
+  );
 }
--- app/[lang]/portfolio/page.tsx
+++ app/[lang]/portfolio/page.tsx
@@ -1,28 +1,31 @@
-import { Metadata } from 'next'
-import PortfolioPage from './portfolio-page'
-import { TLocale, TMetadataProps } from '../../../src/entities'
-import { getDictionary } from '../dictionaries'
+import { Metadata } from 'next';
+import PortfolioPage from './portfolio-page';
+import { TLocale, TMetadataProps } from '../../../src/entities';
+import { getDictionary } from '../dictionaries';
 
-export async function generateMetadata(
-  { params }: TMetadataProps,
-): Promise<Metadata> {
-  const dict = await getDictionary(params.lang)
+export async function generateMetadata({
+  params,
+}: TMetadataProps): Promise<Metadata> {
+  const dict = await getDictionary(params.lang);
   return {
     title: dict['translation']['meta']['portfolio_title'],
     description: dict['translation']['meta']['portfolio_description'],
     openGraph: {
       images: [dict['translation']['meta']['og_images']],
     },
-  }
+    alternates: {
+      canonical: '/portfolio',
+    },
+  };
 }
 
 export default function Page(params: { params: { lang: TLocale } }) {
-    // TODO: params is added by next.js
-  const locale = (params as any).params.lang as TLocale
+  // TODO: params is added by next.js
+  const locale = (params as any).params.lang as TLocale;
   return (
     <>
       {/* @ts-expect-error Server Component */}
-      <PortfolioPage lang={locale}/>
+      <PortfolioPage lang={locale} />
     </>
-  )
+  );
 }
